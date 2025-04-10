e0295924f0c7d0bc6e71368d89d245a529dc4044
jasnael94 <jasnael94@gmail.com>
2025-04-10 10:08:28 +0200

    edit project

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+import { Camera, Icon } from 'lucide-react';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <Icon as={Camera} size="md" />,
+        }}
+      />
+      
+    </Tabs>
+  );
+}
